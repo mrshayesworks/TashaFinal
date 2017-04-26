3a38877cf66cc25a5b6a343302f172865874c0aa
Danielle Hayes <MrsHayesworks@gmail.com>
2017-04-26 00:09:55 -0400

    Update sendemail.php

--- sendemail.php
+++ sendemail.php
@@ -9,20 +9,7 @@
 		
 		$body = "From: $name\n E-Mail: $email\n Message:\n $message";
  
-		// Check if name has been entered
-		if (!$_POST['name']) {
-			$errName = 'Please enter your name';
-		}
-		
-		// Check if email has been entered and is valid
-		if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-			$errEmail = 'Please enter a valid email address';
-		}
-		
-		//Check if message has been entered
-		if (!$_POST['message']) {
-			$errMessage = 'Please enter your message';
-		}
+	
 // If there are no errors, send the email
 if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
 	if (mail ($to, $subject, $body, $from)) {
